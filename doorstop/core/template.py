ce9d050756fc935d8b1611e00e50070c878f0192
Felix Eckstein (Ext.) <Felix.Eckstein@sieb-meyer.de>
2026-06-08 09:31:40 +0200

    updated tests

--- doorstop/core/template.py
+++ doorstop/core/template.py
@@ -69,15 +69,10 @@ I.e., only one of the documents in the tree should have a template folder."""
 
     # Check for custom template and verify that it is available.
     if template and not document_template:
-        # log an error warning and stick to the builtin template
-        # raise common.DoorstopWarning(
-        #     "Template flag set, but no 'template' folder was found."
-        # )
-        log.error(
-            "Template flag set, but no 'template' folder was found. Standard template is used instead."
+        raise common.DoorstopError(
+            "Template flag set, but no 'template' folder was found."
         )
-        template = None
-
+    
     # Get the builtin templates.
     template_assets = os.path.join(os.path.dirname(__file__), "files", "templates")
     builtin_template = None
